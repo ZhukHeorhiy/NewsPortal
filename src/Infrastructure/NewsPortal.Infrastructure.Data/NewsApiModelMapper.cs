1cb11e3522e7a0f605a81d325e5e1feaa0d1c3ce
Zhuk Heorhiy <zhuk.george.ukr@gmail.com>
2022-07-17 16:09:58 +0200

    Commit 5

---
+++ src/Infrastructure/NewsPortal.Infrastructure.Data/NewsApiModelMapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewsPortal.Domain;
+
+namespace NewsPortal.Infrastructure.Data
+{
+    internal static class NewsApiModelMapper
+    {
+        public static IEnumerable<News> ToNewsList(this IEnumerable<NewsApiModel> newsApiList)
+        {
+            return newsApiList.Select(news => new News(
+                news.Articles.Author,
+                news.Articles.Title,
+                news.Articles.Description,
+                news.Articles.Url,
+                news.Articles.UrlToImage,
+                news.Articles.PublishedAt,
+                news.Articles.Content
+                )
+            );
+        }
+
+
+    }
+}
